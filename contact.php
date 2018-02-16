e8d50a77c81abb20b50ec30c87e7ffdc102595d4
cat-eat-fish <35954293+cat-eat-fish@users.noreply.github.com>
2018-02-16 12:34:06 +0800

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+	//获取数据
+	$youremail = trim(htmlspecialchars($_POST['your_email']));
+	$yourname = stripslashes(strip_tags($_POST['your_name']));
+    $yourmessage = stripslashes(strip_tags($_POST['your_message']));
+
+    $str=$youremail.','.$yourname.','.$yourmessage;
+
+    $fh=fopen('hello.txt', 'a');
+	fwrite($fh,$str);
+	fclose($fh);
+	echo "发送成功！";
+	echo "<br/>";
+	echo "<a href='contact.html'>返回</a>";
+ ?>
